32d08d4accd9b377496e2be4b4542715eb36f718
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-06-29 21:04:12 +0500

    setup files, create superuser

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,9 +4,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
