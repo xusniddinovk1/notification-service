df76ed8120ede05818926a5a8a2e931d27f56498
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-06-30 11:23:56 +0500

    add custom user model

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -5,8 +5,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
