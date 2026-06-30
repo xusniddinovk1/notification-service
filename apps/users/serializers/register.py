8d37346bbbdfbd5fa1c9856da83037f2576b3758
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-06-30 11:39:19 +0500

    add register serializer

---
+++ apps/users/serializers/register.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from apps.users.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ["email", "password", "first_name", "last_name"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
