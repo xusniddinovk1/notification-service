ecf09ea1f220fd1d4d1cf71facdba407f99c362f
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-07-02 17:24:59 +0500

    add celery

--- config/settings/base.py
+++ config/settings/base.py
@@ -120,3 +120,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SWAGGER_URL = "/api/schema/swagger-ui/"
+
+CELERY_BROKER_URL = os.getenv('REDIS_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', default='redis://localhost:6379/0')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Tashkent'
